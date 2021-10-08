3bd88083a581c9f6eb98df0bf82a820199468e25
fatai2 <31743893+fatai2@users.noreply.github.com>
2021-10-07 20:55:59 -0400

    Update chevron.js

--- chevron.js
+++ chevron.js
@@ -1,3 +1,17 @@
+
+/* My addition to allow tooltips */
+$(".more_info").click(function () {
+    var $title = $(this).find(".title");
+    if (!$title.length) {
+        $(this).append('<span class="title">' + $(this).attr("title") + '</span>');
+    } else {
+      $($title).fadeOut(250, function() {
+          $title.remove();
+      });
+    }
+});
+
+
 /* Collapsible Modules */
 
 /* Returns a list of collapsed modules, from the 'collapse' cookie */
